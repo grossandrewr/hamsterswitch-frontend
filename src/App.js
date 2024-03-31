57b10a080a744f671809b0a619e835ba18011e8b
Andrew Gross <gross.andrewr@gmail.com>
2024-03-31 11:24:19 +0200

    Setup Spotify API flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { 
+  redirectToAuthCodeFlow,
+  getAccessToken,
+  fetchProfile
+ } from './auth.js'  
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+
+  const [profile, setProfile] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const accessToken = await getAccessToken(clientId, code);
+      if (accessToken) {
+        setAccessToken(accessToken)
+      }
+      const profileData = await fetchProfile(accessToken);
+      if (profileData && profileData.email) {
+        setProfile(profileData);
+      }
+    }
+
+    if (profile && profile.email) {
+      return;
+    } else if (!code) {
+      redirectToAuthCodeFlow(clientId);
+    } else {
+      fetchData();
+    }
+  }, [clientId, code]);
+
+  useEffect(() => {
+    if (!profile) return;
+    if (profile.images[0]) {
+      const profileImage = new Image(100, 100);
+      profileImage.src = profile.images[0].url;
+      document.getElementById("avatar").appendChild(profileImage);
+      document.getElementById("imgUrl").innerText = profile.images[0].url;
+    }
+  }, [profile]) 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    profile && profile.email ? 
+      <>
+        <h1>Display your Spotify profile data</h1>
+        <section id="profile">
+        <h2>Logged in as <span id="displayName"></span></h2>
+        <span id="avatar"></span>
+        <ul>
+            <li>User ID: <span id="id">{profile.id}</span></li>
+            <li>Email: <span id="email">{profile.email}</span></li>
+            <li>Spotify URI: <a id="uri" href={profile.external_urls.spotify}></a></li>
+            <li>Link: <a id="url" href={profile.href}>{profile.href}</a></li>
+            <li>Profile Image: <span id="imgUrl"></span></li>
+        </ul>
+        </section>
+      </>
+    : <h1>Loading...</h1>
+    
+
   );
 }
 
